8effc945e6204a1b0b71d587ee464ccaf7ae0979
Samiksha Sharma <sharmasamiksha189@gmail.com>
2026-08-08 15:13:19 +0530

    Setting-up Fontend Structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import LandingPage from './pages/landing';
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <> 
+//     <Router>
+
+
+//       <Routes>
+
+//         {/* <Route path='/home' element= /> */}
+//         <Route path='/' element= {<LandingPage></LandingPage>} />
+//       </Routes>
+//     </Router>
+//     </>
+//   )
+// }
+
+// export default App
---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,45 @@
+// import React from 'react'
+// import "../App.css"
+
+// export default function LandingPage(){
+//     return(
+//         <div className= 'landingPageContainer'>
+//             <nav>
+//                 <div className= 'navHeader'>
+//                     <h2>Apna Video Call</h2>
+//                 </div>
+//                 <div className= 'navlist'>
+//                     <p>Join as Guest</p>
+//                     <p>Register</p>
+//                     {/* <button>Login</button> */}
+//                     <div roll= 'button'>
+//                         <p>Login</p>
+//                     </div>
+//                 </div>
+//             </nav>
+
+//             <div className= "landingMainContainer">
+//                 <div>
+//                     <h1><span style= {{ color: 'darkorange'}}>Connect</span> with your Loved Ones</h1>
+
+//                     <p>Cover the distance by Apna Video Call</p>
+//                     <div roll= 'button'>
+//                         <Link to= {"/home"} >Get Started</Link>
+//                     </div>
+//                 </div>
+//                 <div>
+
+//                     <img src="/mobile.png" alt= "Mobile"/>
+//                 </div>
+//             </div>
+
+
+
+
+//         </div>
+//     )
+
+// }
+
+// // -> CSS TO JSX
+// // -> PREMADE COMPONENTS
