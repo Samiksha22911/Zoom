7a0b7aad1fe9ff220d8dc465f2ed79a12386a9d7
Samiksha Sharma <sharmasamiksha189@gmail.com>
2026-08-08 15:19:47 +0530

    Add frontend CSS styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,27 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-// import './App.css'
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import LandingPage from './pages/landing';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/landing';
 
-// function App() {
-//   const [count, setCount] = useState(0)
+function App() {
+  const [count, setCount] = useState(0)
 
-//   return (
-//     <> 
-//     <Router>
+  return (
+    <> 
+    <Router>
 
 
-//       <Routes>
+     <Routes>
 
-//         {/* <Route path='/home' element= /> */}
-//         <Route path='/' element= {<LandingPage></LandingPage>} />
-//       </Routes>
-//     </Router>
-//     </>
-//   )
-// }
+       {/* <Route path='/home' element= /> */}
+        <Route path='/' element= {<LandingPage></LandingPage>} />
+     </Routes>
+    </Router>
+    </>
+  )
+}
 
 // export default App
